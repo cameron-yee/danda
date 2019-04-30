8a9cdf9d2d6b023507a4f57289c3607267b56797
cameron-yee <yee.camero23@gmail.com>
2019-04-29 21:30:00 -0600

    Edits to insert sort

--- a/insert_sort.c
+++ a/insert_sort.c
@@ -17,7 +17,7 @@ void printList(int *list, size_t list_length) {
 int *get_sorted_list(int *list, size_t list_length) {
     //int sorted_list[list_length] = (int *)malloc(sizeof(int * list_length));
     int *sorted_list;
-    
+
     sorted_list = (int *)malloc(sizeof(int) * list_length);
 
     if(sorted_list == NULL) {
@@ -30,25 +30,34 @@ int *get_sorted_list(int *list, size_t list_length) {
     size_t y;
 
     for(y = 0; y < list_length; y++) {
+        //printf("y%lu ", y);
         sorted_list[y] = -1;
     }
 
+    printf("%lu\n", list_length);
+
     for(i = 0; i < list_length; i++) {
         for(x = 0; x < list_length; x++) {
+            printf("%d ", sorted_list[x]);
             if(sorted_list[x] == -1) {
+                //printf("x1[%lu] ", x);
                 sorted_list[x] = list[i];
                 break;
             } else if(sorted_list[x+1] != -1) {
+                //printf("x2[%lu] ", x);
                 continue;
-            } else if (sorted_list[x] > list[i]) {
+            } else if(sorted_list[x] > list[i]) {
+                //printf("x3[%lu] ", x);
                 sorted_list[x+1] = sorted_list[x];
                 sorted_list[x] = list[i];
                 break;
-            } else if (sorted_list[x] <= list[i]) {
+            } else if(sorted_list[x] <= list[i]) {
+                //printf("x4[%lu] ", x);
                 sorted_list[x+1] = list[i];
                 break;
             }
         }
+        printf("\n");
     }
 
     return sorted_list;
@@ -62,13 +71,18 @@ int main() {
 
     numbers_length = sizeof(numbers)/sizeof(numbers[0]);
 
-    printList(numbers, numbers_length);
+    printf("%lu\n", numbers_length);
+
+    //printList(numbers, numbers_length);
 
     int *sorted = get_sorted_list(numbers, numbers_length);
 
     sorted_length = sizeof(sorted)/sizeof(sorted[0]);
 
-    printList(sorted, sorted_length);
+    //printf("%lu\n", sorted_length);
+
+    //printList(sorted, sorted_length);
+    printList(sorted, 9);
 
     return 0;
 }
