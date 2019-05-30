58dd93227de22c5c56b5e2f7484fac4cc3ff8f21
cameron-yee <yee.camero23@gmail.com>
2019-05-29 19:56:22 -0600

    Misc naming edits to doubly_linked_list

--- d/doubly_linked_list.h
+++ d/doubly_linked_list.h
@@ -1,41 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifndef DOUBLY_LINKED_LIST_H
+#define DOUBLY_LINKED_LIST_H
+
 struct Node {
     int data;
-    struct Node* next;
-    struct Node* prev;
+    struct Node *next;
+    struct Node *prev;
 };
 
-void node_delete(struct Node* node);
-void insert_at_head(struct Node** node, int data);
-void insert_at_position(struct Node**, int data, int index);
-void insert_at_tail(struct Node** head, int data);
-void delete_head(struct Node** head);
-void delete_tail(struct Node* head);
-void delete_at_position(struct Node* head, int position);
-void print(struct Node* head);
-void reverse_print(struct Node* head);
+void node_delete(struct Node *node);
+void insert_at_head(struct Node **node, int data);
+void insert_at_position(struct Node **node, int data, int index);
+void insert_at_tail(struct Node **head, int data);
+void delete_head(struct Node **head);
+void delete_tail(struct Node *head);
+void delete_at_position(struct Node *head, int position);
+void print(struct Node *head);
+void reverse_print(struct Node *head);
 
 
-struct Node* node_new(int data) {
-    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node)); //malloc allocates memory on the heap so the data will persist
+struct Node *node_new(int data) {
+    struct Node *new_node;
+
+    new_node = (struct Node*)malloc(sizeof(struct Node)); //malloc allocates memory on the heap so the data will persist
+
+    if (new_node == NULL) {
+        printf("Error in malloc.");
+        exit(1);
+    }
+
     new_node->data = data;
     new_node->prev = NULL;
     new_node->next = NULL;
     return new_node;
 };
 
-void node_delete(struct Node* node) {
+void node_delete(struct Node *node) {
     if(node != NULL) {
         free(node);
         node = NULL;
     }
 }
 
-void insert_at_head(struct Node** head, int data) {
-    struct Node* new_node = node_new(data);
-    if(head == NULL) {
+void insert_at_head(struct Node **head, int data) {
+    struct Node *new_node;
+
+    new_node = node_new(data);
+
+    if (head == NULL) {
         *head = new_node;
         return;
     }
@@ -46,8 +60,8 @@ void insert_at_head(struct Node** head, int data) {
     //return head;
 }
 
-void insert_at_position(struct Node** head, int data, int index) {
-    struct Node* temp;
+void insert_at_position(struct Node **head, int data, int index) {
+    struct Node *temp, *new_node;
     int current_position; 
 
     temp = (*head);
@@ -61,7 +75,7 @@ void insert_at_position(struct Node** head, int data, int index) {
     current_position = 0;
     while(temp->next != NULL) {
         if(current_position == index) {
-            struct Node* new_node = node_new(data);
+            new_node = node_new(data);
             new_node->prev = temp->prev;
             new_node->prev->next = new_node;
             new_node->next = temp;
@@ -79,9 +93,9 @@ void insert_at_position(struct Node** head, int data, int index) {
     printf("Index out of range.");
 }
 
-void insert_at_tail(struct Node** head, int data) {
-    struct Node* new_node;
-    struct Node* temp;
+void insert_at_tail(struct Node **head, int data) {
+    struct Node *new_node;
+    struct Node *temp;
 
     new_node = node_new(data);
     temp = (*head);
@@ -116,8 +130,8 @@ void delete_head(struct Node** head) {
     node_delete(old_head);
 }
 
-void delete_tail(struct Node* head) {
-    struct Node* temp;
+void delete_tail(struct Node *head) {
+    struct Node *temp;
     temp = head;
 
     while(temp->next != NULL) {
@@ -129,8 +143,8 @@ void delete_tail(struct Node* head) {
     node_delete(temp->next);
 }
 
-void delete_at_position(struct Node* head, int position) {
-    struct Node* temp;
+void delete_at_position(struct Node *head, int position) {
+    struct Node *temp, *new_next;
     int current_position;
 
     temp = head;
@@ -146,7 +160,7 @@ void delete_at_position(struct Node* head, int position) {
         return;
     } else if(current_position == position) {
         temp = temp->prev;
-        struct Node* new_next = temp->next->next;
+        new_next = temp->next->next;
         node_delete(temp->next);
         temp->next=new_next;
         new_next->prev = temp;
@@ -156,8 +170,8 @@ void delete_at_position(struct Node* head, int position) {
     }
 }
 
-void print(struct Node* head) {
-    struct Node* temp;
+void print(struct Node *head) {
+    struct Node *temp;
     temp = head;
 
     printf("Forward: ");
@@ -168,8 +182,8 @@ void print(struct Node* head) {
     printf("\n");
 }
 
-void reverse_print(struct Node* head) {
-    struct Node* temp;
+void reverse_print(struct Node *head) {
+    struct Node *temp;
     temp = head;
 
     if(temp == NULL) return;
@@ -184,3 +198,4 @@ void reverse_print(struct Node* head) {
     printf("\n");
 }
 
+#endif
